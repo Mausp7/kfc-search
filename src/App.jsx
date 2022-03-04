4b157b420075184f481feffef72ffd823ea24db0
Mausp7 <archie.tombacz@gmail.com>
2022-03-04 14:37:42 +0100

    Add Material-UI.

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,84 @@
 import { useState } from "react";
-
+import { FormControl, FormLabel, FormControlLabel, RadioGroup, Radio, /* StaticDatePicker,  */TextField, Button} from "@mui/material";
+/* import DateAdapter from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+ */
 function App() {
   const presentDate = new Date().toISOString().split("T")[0];
 
   const [orderScource, setOrderScource] = useState("W");
   const [date, setDate] = useState(presentDate);
   const [code, setCode] = useState("");
-  const [frameScource, setFrameScource] = useState("");
 
-
-  const link = `https://hu.eu.logisticsbackoffice.com/dispather/order_details/${orderScource}%2FD%2F${date.split("-").join("")}%2F${code}`;
-  const altLink = `https://hu.usehurrier.com/dispather/order_details/${orderScource}%2FD%2F${date.split("-").join("")}%2F${code}`
+  const link = `https://hu.eu.logisticsbackoffice.com/dispatcher/order_details/${orderScource}%2FD%2F${date.split("-").join("")}%2F${code}`;
+  const altLink = `https://hu.usehurrier.com/dispatcher/order_details/${orderScource}%2FD%2F${date.split("-").join("")}%2F${code}`
 
   return (
     <div className="App">
-      <h1>KFC saját oldalas rendelés kereső</h1>
-      <input 
-        type="text"
-        required
-        value={orderScource}
-        onChange={event => setOrderScource(event.target.value)}
-      />
-      <input 
-        type="date" 
-        required
+      <h1>KFC saját oldalas rendelés</h1>
+      <h2>Hurrier link generátor</h2>
+      <FormControl>
+        <FormLabel id="radio-label">Rendelési Platform:</FormLabel>
+        <RadioGroup
+          aria-labelledby="Rendelés feladási platform:"
+          value={orderScource}
+          onChange={(event) => setOrderScource(event.target.value)}
+        >
+          <FormControlLabel value="W" control={<Radio />} label="Web App" />
+          <FormControlLabel value="M" control={<Radio />} label="Mobile App" />
+        </RadioGroup>
 
-        max={presentDate}
-        value={date}
-        onChange={event => setDate(event.target.value)}
+{/*         <LocalizationProvider dateAdapter={DateAdapter}>
+          <StaticDatePicker
+            label="Basic example"
+            orientation="landscape"
+            openTo="day"
+            value={date}
+            onChange={event => setDate(event.target.value)}
+            renderInput={(params) => <TextField {...params} />}
+          />
+        </LocalizationProvider>
+ */}      
+        <input 
+          type="date" 
+          required
+          
+          max={presentDate}
+          value={date}
+          onChange={event => setDate(event.target.value)}
+        />
+      <TextField
+              type="text"
+              label="Rendelésazonosító"
+              placeholder="pl: ABCDEFG"
+              value={code}
+              onChange={event => setCode(event.target.value.toUpperCase())}
+              variant="outlined"
       />
-      <input 
-        type="text"
-        required
+    </FormControl>
 
-        value={code}
-        onChange={event => setCode(event.target.value)}
-      />
-      <p>{link}</p>
-      <button onClick={() => setFrameScource(link)}>Mutat</button>
-      <p>{altLink}</p>
-      <button onClick={() => setFrameScource(altLink)}>Mutat</button>
-
-      <iframe src={`ˇ${frameScource}ˇ`} title="result-iframe" frameBorder="0"></iframe>
-      <a href={frameScource} target="_blank" rel="noreferrer" ><button>Megnyit</button></a>
+      <p>
+        <a href={link} target="_blank" rel="noreferrer" >
+          <Button 
+            variant="contained"
+            disabled={code.length !== 7}
+          >
+            Megnyit
+          </Button>
+        </a>
+        {link}
+      </p>
+      <p>
+        <a href={altLink} target="_blank" rel="noreferrer" >
+          <Button 
+            variant="contained"
+            disabled={code.length !== 7}
+          >
+            Megnyit
+          </Button>
+        </a>
+        {altLink}
+      </p>
     </div>
   );
 }

28f4c9f1550657f9cbda4c164c0cc713bbd5197f
Mausp7 <archie.tombacz@gmail.com>
2022-03-04 17:05:20 +0100

    Adds MUI Date Picker and form validations.

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,30 @@
 import { useState } from "react";
-import { FormControl, FormLabel, FormControlLabel, RadioGroup, Radio, /* StaticDatePicker,  */TextField, Button} from "@mui/material";
-/* import DateAdapter from '@mui/lab/AdapterDateFns';
+import { FormControl, FormLabel, FormControlLabel, RadioGroup, Radio, TextField, Button} from "@mui/material";
+import DateAdapter from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
- */
+import {DatePicker} from '@mui/lab'
+import logo from './resources/KFC-logo.png'
+
 function App() {
-  const presentDate = new Date().toISOString().split("T")[0];
+  const presentDate = new Date();
 
   const [orderScource, setOrderScource] = useState("W");
   const [date, setDate] = useState(presentDate);
   const [code, setCode] = useState("");
 
-  const link = `https://hu.eu.logisticsbackoffice.com/dispatcher/order_details/${orderScource}%2FD%2F${date.split("-").join("")}%2F${code}`;
-  const altLink = `https://hu.usehurrier.com/dispatcher/order_details/${orderScource}%2FD%2F${date.split("-").join("")}%2F${code}`
+  const link = `https://hu.eu.logisticsbackoffice.com/dispatcher/order_details/${orderScource}%2FD%2F${date.toISOString().split("T")[0].split("-").join("")}%2F${code}`;
+  const altLink = `https://hu.usehurrier.com/dispatcher/order_details/${orderScource}%2FD%2F${date.toISOString().split("T")[0].split("-").join("")}%2F${code}`
 
   return (
     <div className="App">
+      <header>
+      <img src={logo} alt="logo" />
       <h1>KFC saját oldalas rendelés</h1>
+      </header>
       <h2>Hurrier link generátor</h2>
+
       <FormControl>
+
         <FormLabel id="radio-label">Rendelési Platform:</FormLabel>
         <RadioGroup
           aria-labelledby="Rendelés feladási platform:"
@@ -28,34 +35,32 @@ function App() {
           <FormControlLabel value="M" control={<Radio />} label="Mobile App" />
         </RadioGroup>
 
-{/*         <LocalizationProvider dateAdapter={DateAdapter}>
-          <StaticDatePicker
-            label="Basic example"
+        <LocalizationProvider dateAdapter={DateAdapter}>
+          <DatePicker
+            label="Order date"
             orientation="landscape"
-            openTo="day"
             value={date}
-            onChange={event => setDate(event.target.value)}
+            maxDate={presentDate}
+            required
+            onChange={newValue => setDate(newValue)}
             renderInput={(params) => <TextField {...params} />}
           />
         </LocalizationProvider>
- */}      
-        <input 
-          type="date" 
-          required
-          
-          max={presentDate}
-          value={date}
-          onChange={event => setDate(event.target.value)}
+
+        <TextField
+                type="text"
+                label="Rendelésazonosító"
+                placeholder="pl: ABCDEFG"
+                helperText={code.length !== 7 ? "7 karakter hosszúságú" : null}
+                error={code.length > 7}
+                value={code}
+                onChange={event => setCode(event.target.value.toUpperCase())}
+                variant="outlined"
         />
-      <TextField
-              type="text"
-              label="Rendelésazonosító"
-              placeholder="pl: ABCDEFG"
-              value={code}
-              onChange={event => setCode(event.target.value.toUpperCase())}
-              variant="outlined"
-      />
-    </FormControl>
+
+      </FormControl>
+
+      <hr />
 
       <p>
         <a href={link} target="_blank" rel="noreferrer" >
